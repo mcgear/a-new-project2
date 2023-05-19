8f21df594a17247eef8076c0f9edb7c2eb6d75e4
michael.gearhardt@fathym.com <michael.gearhardt@fathym.com>
2023-05-19 22:01:33 +0000

    Update project with new license, form validation, layout, import statement, and button styles.

--- README.md
+++ README.md
@@ -1,29 +1,3 @@
-## A New Project
+# A New Project
 
-This is a new project that does amazing things.
-
-### Installation
-
-To install this project, follow these steps:
-
-1. Clone the repository.
-2. Run `npm install`.
-3. Run `npm start`.
-
-### Usage
-
-To use this project, follow these steps:
-
-1. Do amazing things with it.
-
-### Contributing
-
-To contribute to this project, follow these steps:
-
-1. Fork the repository.
-2. Make your changes.
-3. Submit a pull request.
-
-### License
-
-This project is licensed under the MIT License.
+This is a sample project for demonstrating how to use Fathym Low-Code Framework.
--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,23 +1,116 @@
-import React from 'react';
+```
+import React, { useState } from 'react';
 
-function SignUpForm() {
+const SignUpForm = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [errors, setErrors] = useState({});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const errors = validateForm();
+    if (Object.keys(errors).length === 0) {
+      console.log('Form submitted successfully!');
+      setFirstName('');
+      setLastName('');
+      setEmail('');
+      setPassword('');
+      setConfirmPassword('');
+    } else {
+      setErrors(errors);
+    }
+  };
+
+  const validateForm = () => {
+    const errors = {};
+    if (!firstName.trim()) {
+      errors.firstName = 'First name is required';
+    }
+    if (!lastName.trim()) {
+      errors.lastName = 'Last name is required';
+    }
+    if (!email.trim()) {
+      errors.email = 'Email is required';
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      errors.email = 'Email address is invalid';
+    }
+    if (!password.trim()) {
+      errors.password = 'Password is required';
+    } else if (password.length < 6) {
+      errors.password = 'Password must be at least 6 characters';
+    }
+    if (!confirmPassword.trim()) {
+      errors.confirmPassword = 'Confirm password is required';
+    } else if (password !== confirmPassword) {
+      errors.confirmPassword = 'Passwords do not match';
+    }
+    return errors;
+  };
+
   return (
-    <form>
-      <label>
-        Name:
-        <input type="text" name="name" />
-      </label>
-      <label>
-        Email:
-        <input type="email" name="email" />
-      </label>
-      <label>
-        Password:
-        <input type="password" name="password" />
-      </label>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="firstName">First Name:</label>
+        <input
+          type="text"
+          id="firstName"
+          name="firstName"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        {errors.firstName && <span>{errors.firstName}</span>}
+      </div>
+      <div>
+        <label htmlFor="lastName">Last Name:</label>
+        <input
+          type="text"
+          id="lastName"
+          name="lastName"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        {errors.lastName && <span>{errors.lastName}</span>}
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        {errors.email && <span>{errors.email}</span>}
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {errors.password && <span>{errors.password}</span>}
+      </div>
+      <div>
+        <label htmlFor="confirmPassword">Confirm Password:</label>
+        <input
+          type="password"
+          id="confirmPassword"
+          name="confirmPassword"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        {errors.confirmPassword && <span>{errors.confirmPassword}</span>}
+      </div>
       <button type="submit">Submit</button>
     </form>
   );
-}
+};
 
 export default SignUpForm;
+```
